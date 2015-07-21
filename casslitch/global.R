29f7838266dea6087f24fcd86405c9aa86a16ba3
casslitch <casslitch@gmail.com>
2015-07-21 12:40:00 +1000

    msg

--- casslitch/global.R
+++ casslitch/global.R
@@ -1,9 +1,5 @@
-#Author: Cassandra Litchfield
-#clitchfield@student.unimelb.edu.au
-#July 2015
-
 library(biomaRt)
-ensembl = useMart("ensembl",dataset="scerevisiae_gene_ensembl")
+ensembl = useMart("ensembl",dataset="celegans_gene_ensembl")
 
 #csvs
 df_info<-read.csv("genewise-info.csv")
@@ -49,7 +45,7 @@ colnames(df3)<-col_nom
 
 
 #take log2 of all coloumns
-genewise_exp<-log2(df3[,]+1)
+genewise_exp<-log2(df3[,]+0.5)
 
 
 #tail lengths
@@ -57,7 +53,7 @@ df_rawgene_tail<-data.frame(raw_gene_tail,  stringsAsFactors = T)
 df4<-df_rawgene_tail[,-1]
 rownames(df4)<-df_rawgene_tail[,1]
 
-#calculate means for each sample (tail lengths)
+#calculate means for each sample
 for(i in samples){
   reps<-subset(colnames(df3),grepl(paste0(i,".rep"),colnames(df3)))
   df8<-subset(df4, select=reps)
@@ -69,10 +65,10 @@ colnames(df4)<-col_nom
 genewise_tail_length<-df4
 
 
-#drop down boxes
+#dropbox names
 check_boxes<-names(df3)
 
-#returns genes with given product description key term
+#product description search
 get_genes<-function(key_term,dfinfo,colu) {
   genes_bool<-rep(F,nrow(dfinfo))
   for(i in 1:nrow(dfinfo)){
@@ -91,8 +87,52 @@ get_genes<-function(key_term,dfinfo,colu) {
   return(genes)
 }
 
-#returns genes for given GO_term
+#returns gene names for given GOterm
 get_genes_GO<-function(GO_term){
-  genes<-getBM(attributes = c("ensembl_gene_id") , filters = c("go_id"), values = GO_term , mart = ensembl)
+  genes<-getBM(attributes = c("refseq_mrna") , filters = c("go_id"), values = GO_term , mart = ensembl)
   return(genes)
 }
+
+#for plotting genes listed in "887 RBPs- TableS2.csv"
+file<-read.csv("887 RBPs- TableS2.csv")
+gene_id<-lapply(file[,"WBID..WS219."],as.character)
+
+#based on WB ids in file, find either NM code or ORF
+genes<-getBM(attributes = c("ensembl_gene_id","refseq_mrna","wormbase_gene_seq_name") , filters = c("ensembl_gene_id"), values=gene_id, mart = ensembl)
+
+#which WB ids couldn't find NM code or ORF for
+not_found<-c()
+for(i in gene_id){
+  if(!(i %in% genes[,1]))
+  not_found<-c(not_found,i)
+}
+
+#using ORF from our file, does it match genewise info file? - nope. Ignore.
+for(i in not_found){
+  for(a in df_info$gene)
+    if(grepl(i,a))
+      print("Y")
+}
+
+#1 means NR matches NR in our data
+for(i in 1:length(genes$refseq_mrna)){
+  if(!(genes[i,"refseq_mrna"] %in% df_info$Name))
+    genes[i,"NR"]<-0
+  else
+    genes[i,"NR"]<-1
+}
+
+#1 means ORF matches ORF in our data
+for(i in 1:length(genes$wormbase_gene_seq_name)){
+  if(!(genes[i,"wormbase_gene_seq_name"] %in% df_info$gene))
+    genes[i,"ORF"]<-0
+  else
+    genes[i,"ORF"]<-1
+}
+
+
+#NR match df
+NR_df<-subset(genes,genes$NR==1)
+
+#ORF match df
+ORF_df<-subset(genes,genes$ORF==1&genes$refseq_mrna=="")
--- casslitch/server.R
+++ casslitch/server.R
@@ -4,24 +4,24 @@ library(biomaRt)
 
 
 shinyServer(function(input,output,session){
-  #make inputs reactive
-  selectionX<-reactive({
-    return(input$sampleX)
-  })
+  #turning shiny user inputs into variables
+   selectionX<-reactive({
+      return(input$sampleX)
+    })
+    
+   selectionY<-reactive({
+      return(input$sampleY)
+    })
+    
+   GOt<-reactive({
+      return(input$GOterm)
+    })
   
-  selectionY<-reactive({
-    return(input$sampleY)
-  })
-  
-  GOt<-reactive({
-    return(input$GOterm)
-  })
-  
-  productt<-reactive({
-    return(input$productterm)
-  })
-  
-  searchterm<-reactive({
+   productt<-reactive({
+     return(input$productterm)
+   })
+   
+   searchterm<-reactive({
     return(input$bygene)
   })
   
@@ -33,38 +33,58 @@ shinyServer(function(input,output,session){
     return(input$dataframy)
   })
   
-  #Plots
+  
   output$distPlot <- renderPlot({
-    
-    df1<-genewise_exp
-    dafx<-dataframx()
-    dafy<-dataframy()
-    selX<-selectionX()
-    selY<-selectionY()
-    dfx<-get(dafx)
-    dfy<-get(dafy)
-    
-    GO_data<-function(GOterm){
-      genes<-get_genes_GO(GOterm)
-      df<-data.frame(genes,selectionX=0,selectionY=0, GOterm,row.names=1)
-      colnames(df)<-c(selX,selY,"ID")
       
-      #with isoforms
-      if(input$isoforms){
+#turning shiny user inputs into variables
+      df1<-genewise_exp
+      dafx<-dataframx()
+      dafy<-dataframy()
+      selX<-selectionX()
+      selY<-selectionY()
+      dfx<-get(dafx)
+      dfy<-get(dafy)
+      Gts<-GOt()
+      
+#creating data frame for plotting given GOterm
+      GO_data<-function(GOterm){
+        genes<-get_genes_GO(GOterm)
+        df<-data.frame(genes,selectionX=0,selectionY=0, GOterm,row.names=1)
+        colnames(df)<-c(selX,selY,"ID")
+        
+        #with isoforms
+        if(input$isoforms){
         for(gene in rownames(df)){
           for(i in rownames(dfx)){
             if(grepl(gene,i)){
-              if(i %in% rownames(dfy)){
-                print(i)
-                df[i,1]<- dfx[i,selX]
-                df[i,2]<- dfy[i,selY]
-                df[i,3]<-GOterm
-              }
+                if(i %in% rownames(dfy)){
+                  print(i)
+                  df[i,1]<- dfx[i,selX]
+                  df[i,2]<- dfy[i,selY]
+                  df[i,3]<-GOterm
+               }
             }
           }}}
-      
-      else{
+        
+        else{
         #without isoforms       
+        for(gene in rownames(df)){
+          if ((gene %in% rownames(dfx))){
+            if ((gene %in% rownames(dfy))){
+            df[gene,1]<- dfx[gene,selX]
+            df[gene,2]<- dfy[gene,selY]
+          }
+          }
+        }}
+        return(df)
+      }
+
+#creating data frame for plotting given product term
+      product_data<-function(keyterm){
+        genes<-get_genes(keyterm,df_info,"product")
+        df<-data.frame(genes,selectionX=0,selectionY=0, keyterm,row.names=1)
+        colnames(df)<-c(selX,selY,"key")
+        
         for(gene in rownames(df)){
           if ((gene %in% rownames(dfx))){
             if ((gene %in% rownames(dfy))){
@@ -72,74 +92,90 @@ shinyServer(function(input,output,session){
               df[gene,2]<- dfy[gene,selY]
             }
           }
-        }}
-      return(df)
-    }
-    
-    product_data<-function(keyterm){
-      genes<-get_genes(keyterm,df_info,"product")
-      df<-data.frame(genes,selectionX=0,selectionY=0, keyterm,row.names=1)
-      colnames(df)<-c(selX,selY,"key")
-      
-      for(gene in rownames(df)){
-        if ((gene %in% rownames(dfx))){
-          if ((gene %in% rownames(dfy))){
-            df[gene,1]<- dfx[gene,selX]
-            df[gene,2]<- dfy[gene,selY]
+        }
+        return(df)
+      }
+
+ 
+ #create data frame given file with genes
+      RBP_data<-function(){
+        genes<-lapply(NR_df[,"refseq_mrna"],as.character)
+        genes<-unlist(genes)
+        product<-lapply(df_info[,"gene"],as.character)
+        df<-data.frame(genes,selectionX=0,selectionY=0,row.names=1)
+        colnames(df)<-c(selX,selY)
+        
+        for(i in ORF_df$wormbase_gene_seq_name){
+          for(a in 1:length(product)){
+            if(product[a] == i)
+              genes<-c(genes,df_info[a,"Name"])}
+        }
+        
+        for(gene in rownames(df)){
+          if ((gene %in% rownames(dfx))){
+            if ((gene %in% rownames(dfy))){
+              df[gene,1]<- dfx[gene,selX]
+              df[gene,2]<- dfy[gene,selY]
+            }
           }
         }
+        return(df)
       }
-      return(df)
-    }
-    
-    #xmax
-    xmax<-max(dfx[selX],na.rm = T)
-    #ymax
-    ymax<-max(dfy[selY],na.rm = T)
-    
+      
+    #base plot
     plot_out<-ggplot(df1,aes_string(x=dfx[selX],y=dfy[selY]))+geom_point()
     plot_out<-plot_out+labs(x=paste(dafx,selX),y=paste(dafy,selY))
-    plot_out<-plot_out+geom_segment(aes_string(x=0,y=0,xend=xmax,yend=ymax),colour="blue",size=1.7)
     
-    Gts<-GOt()
     
-    #plot GO term(s)
-    if(Gts!=""){
+      
+    #plot GO terms
+     if(Gts!=""){
       GO_terms<-strsplit(Gts,",")[[1]]
       for(GO_ID in GO_terms){
         df2<-GO_data(GO_ID)
         plot_out<-plot_out+geom_point(data=df2, aes_string(x=df2[1],y=df2[2],colour="ID"))
       }
-    }
-    
-    #plot by product description key term(s)
-    pterm<-productt()
-    if(pterm!=""){
-      p_terms<-strsplit(pterm,",")[[1]]
-      for(pm in p_terms){
-        df5<-product_data(pm)
-        plot_out<-plot_out+geom_point(data=df5, aes_string(x=df5[1],y=df5[2],colour="key"))
       }
-    }
-    
-    #plot by gene/gene family
-    gene1<-searchterm()
-    if(gene1!=""){
-      genes1<-get_genes(gene1,df_info,"gene")
-      dfz<-data.frame(genes1,xax=0,yax=0,row.names=genes1)
-      for(gene in genes1){
-        dfz[gene,"xax"]<-dfx[gene,selX]
-        dfz[gene,"yax"]<-dfy[gene,selY]
-      }
-      plot_out<-plot_out+geom_point(data=dfz,aes_string(x="xax",y="yax"),colour="yellow", size=3.0)
       
-    }
+    #plot product description key terms
+    pterm<-productt()
+      if(pterm!=""){
+        p_terms<-strsplit(pterm,",")[[1]]
+        for(pm in p_terms){
+         df5<-product_data(pm)
+         plot_out<-plot_out+geom_point(data=df5, aes_string(x=df5[1],y=df5[2],colour="key"))
+        }
+        }
     
     
-    plot(plot_out)
-  })
+   #plot adele's data (genes from file)
+    
+    df6<-RBP_data()
+    View(df6)
+    plot_out<-plot_out+geom_point(data=df6,aes_string(x=df6[1],y=df6[2]),colour="red")
+    plot_out<-plot_out+theme_bw()
+    
+    #Blue lines and p-value
+    if(dafy=="genewise_tail_length" & dafx=="genewise_tail_length"){
+      diff_all<-log2(dfx[selX]+0.5)-log2(dfy[selY]+0.5)
+      diff_adele<-log2(df6[selX]+0.5)-log2(df6[selY]+0.5)
+      fold<-rlm(df6[,selX]~df6[,selY],na.action=na.omit)
+      fold<-fold[[1]][2]
+      #print(fold)
+      #fold <- 2**colMeans(diff_all,na.rm=T)[[1]]
+      p_val<-t.test(diff_all[1],diff_adele[1])
+      p_val<-p_val[[3]] #get pvalue
+      print(p_val)
+      plot_out<-plot_out+geom_abline(color="blue",slope=fold, lty=2,size=1)
+      plot_out<-plot_out+geom_abline(colour="blue",size=1.0)
+      plot_out<-plot_out+annotate(geom="text", x=100, y=25, label=paste0("p-value = ",round(p_val,digits=4)), color="black")
+      }
+    
+      plot(plot_out)
+    })
   
-  #download plot
+  
+   #download plot
   output$downloadPlot<-downloadHandler(
     filename = function(){
       "file.pdf"
@@ -149,7 +185,6 @@ shinyServer(function(input,output,session){
       ggsave(file)
     }
   )
-  
 })
 
 
