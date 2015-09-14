fbf7c03c3f0f5b34351948fc8b882ceda151aa5b
BigPatto <andrew.pattison@monash.edu>
2015-09-14 15:43:31 +1000

    Helper is here to help

---
+++ Shiny_tutorial/helper.R
@@ -0,0 +1,14 @@
+make_plot <- function(number_of_points){
+  library("ggplot2")
+  set.seed(5)
+  x <- rnorm(number_of_points, 20, 5)
+  
+  set.seed(10)
+  y <- rnorm(number_of_points, 50,50)
+  
+  df <- data.frame(x,y)
+  
+  ggplot(data=df, aes(x=x, y=y))+ 
+    geom_point()+
+    ggtitle("Awesome Plot")
+}
