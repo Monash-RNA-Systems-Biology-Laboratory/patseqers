52aadd9c4fd990a3d15e69b7b496f41c6862001b
Adele Barugahare <adbarugah@gmail.com>
2016-06-06 10:13:38 +1000

    Updating files prior to expanding app for new dataset

--- adele/dev_timepoint_app/server.R
+++ adele/dev_timepoint_app/server.R
@@ -22,32 +22,33 @@ shinyServer(function(input, output){
     
     #Pull the count data out of the grouped table, filter the values and then normalise with varistran
     dat_count <- reactive({
-        
-        #Retrieve the grouped table reactive object
-        df_count <- file.call()
-        
-        #Pull out the count matrix and the annotations for the count matrix
-        dat_count <- df_count$Count
-        
-        annot <- df_count$Annotation
-        annot_ensem <- rownames(annot)
-        annot_gene <- annot$gene
-        
-        gene_names <- c()
-        for(i in 1:length(annot_ensem)) {
-            gene_names[i] <- paste0(annot_gene[i], "_", annot_ensem[i])
-        }
-        
-        rownames(dat_count) <- gene_names        
-        
-        
-        #Filter  all the columns by the user input
-        dat_count <- dat_count[apply(dat_count, 1, function(row) {all(row >= input$num.filter)}),]
-        
-        #Use varistran to normalise
-        dat_count <- varistran::vst(dat_count)
-        
-        return(dat_count)    
+        withProgress(message = "Loading data", value = 0, {
+            #Retrieve the grouped table reactive object
+            df_count <- file.call()
+            
+            #Pull out the count matrix and the annotations for the count matrix
+            dat_count <- df_count$Count
+            
+            annot <- df_count$Annotation
+            annot_ensem <- rownames(annot)
+            annot_gene <- annot$gene
+            
+            gene_names <- c()
+            for(i in 1:length(annot_ensem)) {
+                gene_names[i] <- paste0(annot_gene[i], "_", annot_ensem[i])
+            }
+            
+            rownames(dat_count) <- gene_names        
+            
+            
+            #Filter  all the columns by the user input
+            dat_count <- dat_count[apply(dat_count, 1, function(row) {all(row >= input$num.filter)}),]
+            
+            #Use varistran to normalise
+            dat_count <- varistran::vst(dat_count)
+            
+            return(dat_count)    
+        })
     })
     
     gene_list <- reactive({
@@ -59,8 +60,8 @@ shinyServer(function(input, output){
     output$gene.select <- renderUI({
         
         selectizeInput("sel.gene", label = h4("Select gene"),
-                    choices = gene_list(),
-                    selected =  gene_list()[1], multiple = FALSE, options = list(maxOptions = 10))  
+                       choices = gene_list(),
+                       selected =  gene_list()[1], multiple = FALSE, options = list(maxOptions = 10))  
         
     })
     
@@ -99,50 +100,53 @@ shinyServer(function(input, output){
     })
     
     mean_count <- reactive({
-        dat_count <- dat_count()
-        new_names <- new_names()
-        replicate_names <- replicate_names()
-        colnames(dat_count) <- new_names
-        
-        mean_dat <- data.frame()
-        
-        for(i in 1:length(replicate_names)) {
-            if(i == 1) {
-                mean_dat <- rowMeans(dat_count[, grep(paste0("^", replicate_names[i], "."), colnames(dat_count))])
-            } else {
-                mean_dat <- cbind(mean_dat, rowMeans(dat_count[, grep(paste0("^", replicate_names[i], "."),
-                                                                      colnames(dat_count))]))
+        withProgress(message = "Calculating the average...", value = 0, {
+            
+            dat_count <- dat_count()
+            new_names <- new_names()
+            replicate_names <- replicate_names()
+            colnames(dat_count) <- new_names
+            
+            mean_dat <- data.frame()
+            
+            for(i in 1:length(replicate_names)) {
+                if(i == 1) {
+                    mean_dat <- rowMeans(dat_count[, grep(paste0("^", replicate_names[i], "."), colnames(dat_count))])
+                } else {
+                    mean_dat <- cbind(mean_dat, rowMeans(dat_count[, grep(paste0("^", replicate_names[i], "."),
+                                                                          colnames(dat_count))]))
+                }
             }
-        }
-        mean_names <- c()
-        for(i in 1:length(replicate_names)) {
-            mean_names[i] <- substr(replicate_names[i], 1, nchar(replicate_names[i]) - 2)
-        }
-        colnames(mean_dat) <- mean_names
-        
-        return(mean_dat)
+            mean_names <- c()
+            for(i in 1:length(replicate_names)) {
+                mean_names[i] <- substr(replicate_names[i], 1, nchar(replicate_names[i]) - 2)
+            }
+            colnames(mean_dat) <- mean_names
+            
+            return(mean_dat)
+        })
     })
     
     summ_stats <- reactive({
         data <- mean_count()
-        timepoints <- as.factor(c(1, 6, 9))
+        Time <- as.numeric(c(1, 6, 9))
         
-        maximuns <- c()
-        minimuns <- c()
+        maximums <- c()
+        minimums <- c()
         df <- data.frame()
         
-        for(i in 1:length(timepoints)){
-            df <- data[, grep(paste0("_", timepointst[i]), colnames(data))]
+        for(i in 1:length(Time)){
+            df <- data[, grep(paste0("_", Time[i]), colnames(data))]
             maximums[i] <- max(df[,1:2])
             minimums[i] <- min(df[,1:2])
             
         }
         
         summ_stats <- data.frame(
-            timepoints,
+            Time,
             maximums,
             minimums
-            )
+        )
         
     })
     
@@ -154,18 +158,39 @@ shinyServer(function(input, output){
         colnames(melted_count)[3] <- "Expression"
         data <- melted_count %>%
             separate(Var2, into = c("Condition", "Time"), sep = "\\_")
-        
         return(data)
     })
     
     #Outputs
     output$time.plot <- renderPlot({
+        withProgress(message = "Generating plot", value = 0, {
+            df <- melted_count()
+            stats <- summ_stats()
+            
+            m <- ggplot(data = stats, aes(x = Time)) 
+            
+            if(input$plot.view == 2) {
+                m <- m + geom_ribbon(aes(ymin = minimums, ymax = maximums), colour = "#cccccc", alpha = 0.2)
+                m <- m + geom_line(data = subset(df, Gene == input$sel.gene), aes(x = as.numeric(Time), 
+                                                                                  y = Expression, group = Condition, 
+                                                                                  colour = Condition), size = 1.5) + 
+                    scale_x_discrete(name = "Time", limits = c(1, 6, 9)) + theme_bw()
+            } else {
+                m <- m + geom_line(data = subset(df, Gene == input$sel.gene), aes(x = as.numeric(Time), 
+                                                                                  y = Expression, group = Condition, 
+                                                                                  colour = Condition),
+                                   size = 1.5) + scale_x_discrete(name = "Time", limits = c(1, 6, 9)) + 
+                    theme_bw()
+            }
+            m
+            
+        })    
+        
+    })
+    
+    output$test.table <- renderTable({
         df <- melted_count()
-        ggplot(data = subset(df, Gene == input$sel.gene), aes(as.factor(Time), Expression, 
-                                                              group = Condition, colour = Condition)) + 
-            geom_line() + theme_bw() + theme(aspect.ratio =1) + xlab("Timepoints (hr)") + 
-            ylab("Gene expression (log2 normalised counts)")
-#         ggplot(data = df, aes(as.factor(Time)))
+        head(df)
     })
     
     output$info.txt <- renderText({
@@ -173,4 +198,32 @@ shinyServer(function(input, output){
         paste("time=", input$plot_click$x, "   expression =", input$plot_click$y)
         
     })
+    
+    output$download.eps <- downloadHandler(
+        filename = function(){
+            paste0(input$sel.gene, Sys.Date(), ".eps")
+        },
+        content = function(file) {
+            ggsave(file)
+        })
+    
+    
+    
 })
+
+
+
+#         geth <- df[df == input$sel.gene,]
+#         subset(df, Gene == input$sel.gene)
+#         ggplot(data = subset(df, Gene == input$sel.gene), aes(as.factor(Time), Expression, 
+#                                                               group = Condition, colour = Condition)) + 
+#             geom_line() +# geom_ribbon(data = stats, aes(ymin = minimums, ymax = maximums), 
+#                          #             colour = "#cccccc", alpha = 0.2) + 
+#         theme_bw() + theme(aspect.ratio =1) + xlab("Timepoints (hr)") + 
+#             ylab("Gene expression (log2 normalised counts)")
+# mop <- ggplot(adf, aes(x = Time)) + geom_ribbon(data = adf, aes(ymin = mn - 1, ymax=mx + 1), colour = "#cccccc",
+#                                                 alpha = 0.2)
+# 
+# 
+# mop + geom_line(data = data_mong[data_mong == "ENSMUSG00000051285",], aes(x = as.numeric(Time), y = Expression, group = Sample,
+#                                                                           colour = Sample), size = 1.5)
--- adele/dev_timepoint_app/ui.R
+++ adele/dev_timepoint_app/ui.R
@@ -7,19 +7,24 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
       selectInput("select.experiment", label = "Select experiment", 
                   choices = list.dirs(full.names=F, recursive =F), 
-                  selected =  list.dirs(full.names=F, recursive =F)[1]),
+                  selected =  list.dirs(full.names=F, recursive =F)[2]),
       radioButtons("org.sel", "Choose which organism to plot", 
                    choices= list("C.albicans" = 1, "Mouse" = 2), 
                    selected = 1),
       helpText("This is important when changing the dataset to look at. If one of the samples is not 
                displaying on the graph, check that the correct organism is selected"),
       numericInput("num.filter", label = "Minimum count filter", value = 25, min = 0),
-      uiOutput("gene.select")
+      uiOutput("gene.select"),
+      radioButtons("plot.view", "View just the gene or the gene of interest in comparison with expression changes
+                   overall", 
+                   choices= list("Only gene of interest" = 1, "Comparison with overall expression change" = 2), 
+                   selected = 1),
+      downloadButton("download.eps", label = "Download eps file")
   ),
   
   mainPanel(
      plotOutput("time.plot", width = "600px", height = "600px", click = "plot_click"),
-     tableOutput("test.table"),
+     #tableOutput("test.table"),
      verbatimTextOutput("info.txt"),
      helpText("You can click on the graph to get X & Y values. Note that the only measured data occur at the given
               timepoints."),
